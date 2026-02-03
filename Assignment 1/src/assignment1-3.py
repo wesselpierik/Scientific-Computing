8abad553ca6f6be176e2f5ccd14b0d2691984571
MikeNieuweboer <nieuweboermike@gmail.com>
2026-02-03 14:34:05 +0100

    Added Geissel and SOR iterations techniques

--- Assignment 1/src/assignment1-3.py
+++ Assignment 1/src/assignment1-3.py
@@ -30,15 +30,18 @@ class BaseGrid:
         self._sinks = np.zeros((grid_size, grid_size), dtype=bool)
         self._grid_size = grid_size
 
-    def step(self) -> None:
+    def step(self) -> float:
+        max_diff = 0
         for row, column in product(
             range(1, self._grid_size - 1),
             range(self._grid_size),
         ):
             if not self._sinks[row, column]:
-                self._step_cell(row, column)
+                diff = self._step_cell(row, column)
+                max_diff = max(max_diff, diff)
+        return max_diff
 
-    def _step_cell(self, row: int, column: int) -> None:
+    def _step_cell(self, _row: int, _column: int) -> float:
         raise NotImplementedError
 
     def add_sink(
@@ -71,6 +74,43 @@ class Jacobi(BaseGrid):
         left = self._state[row, column - 1]
         right = self._state[row, (column + 1) % self._grid_size]
         self._result[row, column] = 0.25 * (up + down + left + right)
+        return self._result[row, column] - self._state[row, column]
+
+
+class GaussSeidel(BaseGrid):
+    def __init__(self, grid_size: int) -> None:
+        super().__init__(grid_size)
+
+    def _step_cell(self, row: int, column: int) -> float:
+        up = self._state[row - 1, column]
+        down = self._state[row + 1, column]
+        left = self._state[row, column - 1]
+        right = self._state[row, (column + 1) % self._grid_size]
+        current = self._state
+        new = 0.25 * (up + down + left + right)
+        diff = current - new
+        self._state[row, column] = new
+        return diff
+
+
+class SOR(BaseGrid):
+    def __init__(self, grid_size: int, omega: float) -> None:
+        super().__init__(grid_size)
+        self._omega = omega
+
+    def _step_cell(self, row: int, column: int) -> float:
+        up = self._state[row - 1, column]
+        down = self._state[row + 1, column]
+        left = self._state[row, column - 1]
+        right = self._state[row, (column + 1) % self._grid_size]
+        current = self._state[row, column]
+        new = (
+            0.25 * self._omega * (up + down + left + right)
+            + (1 - self._omega) * current
+        )
+        diff = current - new
+        self._state[row, column] = new
+        return diff
 
 
 def show_diffusion_step(frame: int, grid: BaseGrid, im: AxesImage) -> list[Artist]:
@@ -84,13 +124,17 @@ def show_diffusion(grid: BaseGrid) -> None:
     axis = fig.subplots(nrows=1)
     im = axis.imshow(grid.state)
     _anim = animation.FuncAnimation(
-        fig, partial(show_diffusion_step, grid=grid, im=im), 100, interval=10, blit=True
+        fig,
+        partial(show_diffusion_step, grid=grid, im=im),
+        100,
+        interval=100,
+        blit=True,
     )
     plt.show()
 
 
 def main() -> None:
-    jacobi = Jacobi(50)
+    jacobi = SOR(50, 1.7)
     show_diffusion(jacobi)
 
 
