41f70879265c4f9f0fa72606b183d15a95ee9579
MikeNieuweboer <nieuweboermike@gmail.com>
2026-02-09 12:40:29 +0100

    Added animations

--- Assignment 1/assignment_2.py
+++ Assignment 1/assignment_2.py
@@ -1,38 +1,13 @@
+from functools import partial
 import math
+from matplotlib.artist import Artist
 from matplotlib.axes import Axes
+from matplotlib.image import AxesImage
+from matplotlib import animation
 import numpy as np
+import numpy.typing as npt
 import matplotlib.pyplot as plt
-
-def main():
-    # boundary conditions
-    c_y0 = 0
-    c_y1 = 1
-
-    # initial conditions
-    c_t0 = 0
-
-    # interval lengths
-    N = 10
-    t0 = 0
-    tN = 10
-    delta_x = 1 / N
-    delta_y = 1 / N
-    delta_t = 0.001
-
-    # parameters
-    D = 1
-
-    # array with x and y
-    c = np.zeros((N, N))
-    c[0,] = c_y1
-
-    stability = stability_condition(delta_t, delta_x, D)
-
-    for t in np.arange(t0, tN, delta_t):
-        c = concentration_timestep(c, delta_x, delta_t, D, N)
-        print(c)
-
-
+import argparse
 
 
 def stability_condition(delta_t, delta_x, D):
@@ -44,20 +19,124 @@ def stability_condition(delta_t, delta_x, D):
     print(f"The stability condition is not satisfied, value is: {stability_check}")
     return False
 
-def concentration_timestep(c, delta_x, delta_t, D, N):
+
+def concentration_timestep(c, delta_x, delta_t, D):
+    N = c.shape[0]
     for x in range(N):
         for y in range(1, N - 1):
             # Boundary condition
             if x == N - 1:
-                c[y, x] = c[y, x] + delta_t * D / (delta_x ** 2) * (c[y, 1] + c[y, x-1] + c[y+1, x] + c[y-1, x] - 4*c[y, x])
+                c[y, x] = c[y, x] + delta_t * D / (delta_x**2) * (
+                    c[y, 1] + c[y, x - 1] + c[y + 1, x] + c[y - 1, x] - 4 * c[y, x]
+                )
 
             elif x == 0:
-                c[y, x] = c[y, x] + delta_t * D / (delta_x ** 2) * (c[y, x+1] + c[y, -2] + c[y+1, x] + c[y-1, x] - 4*c[y, x])
+                c[y, x] = c[y, x] + delta_t * D / (delta_x**2) * (
+                    c[y, x + 1] + c[y, -2] + c[y + 1, x] + c[y - 1, x] - 4 * c[y, x]
+                )
 
             else:
-                c[y, x] = c[y, x] + delta_t * D / (delta_x ** 2) * (c[y, x+1] + c[y, x-1] + c[y+1, x] + c[y-1, x] - 4*c[y, x])
+                c[y, x] = c[y, x] + delta_t * D / (delta_x**2) * (
+                    c[y, x + 1] + c[y, x - 1] + c[y + 1, x] + c[y - 1, x] - 4 * c[y, x]
+                )
 
     return c
+
+
+def plot_analytic(t: float, D: float, N: int, *, axes: Axes | None = None):
+    if axes is None:
+        fig = plt.figure()
+        axes = fig.subplots(nrows=1, ncols=1)
+
+    y_range = np.linspace(0, 1, N)
+    i_max = 100
+    c = [
+        sum(
+            [
+                math.erfc((1 - y + 2 * i) / 2 * math.sqrt(D * t))
+                - math.erfc((1 + y + 2 * i) / 2 * math.sqrt(D * t))
+                for i in range(i_max)
+            ]
+        )
+        for y in y_range
+    ]
+    axes.plot(y_range, c)
+
+
+# Animated grid functions:
+def show_diffusion_step(
+    frame: int,
+    grid: list[npt.NDArray],
+    im: AxesImage,
+    delta_x: float,
+    delta_t: float,
+    D: float,
+) -> list[Artist]:
+    grid[0] = concentration_timestep(
+        grid[0],
+        delta_x,
+        delta_t,
+        D,
+    )
+    im.set_data(grid[0])
+    return [im]
+
+
+def show_diffusion(c: npt.NDArray, delta_x: float, delta_t: float, D: float) -> None:
+    fig = plt.figure()
+    axis = fig.subplots(nrows=1)
+    im = axis.imshow(c)
+    _anim = animation.FuncAnimation(
+        fig,
+        partial(
+            show_diffusion_step, grid=[c], im=im, delta_x=delta_x, delta_t=delta_t, D=D
+        ),
+        100,
+        interval=100,
+        blit=True,
+    )
+    plt.show()
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "option", help="Determine the code to run", choices=["animated"]
+    )
+    return parser.parse_args()
+
+
+def main():
+    parser = parse_args()
+    option = parser.option
+
+    # boundary conditions
+    c_y0 = 0
+    c_y1 = 1
+
+    # interval lengths
+    N = 20
+    t0 = 0
+    tN = 10
+    delta_x = 1 / N
+    delta_t = 0.001
+
+    # parameters
+    D = 1
+
+    # array with x and y
+    c = np.zeros((N, N))
+    c[0,] = c_y1
+
+    stability = stability_condition(delta_t, delta_x, D)
+
+    if option == "animated":
+        show_diffusion(c, delta_x, delta_t, D)
+    else:
+        for t in np.arange(t0, tN, delta_t):
+            c = concentration_timestep(c, delta_x, delta_t, D)
+            print(c)
+
 
 if __name__ == "__main__":
     main()
