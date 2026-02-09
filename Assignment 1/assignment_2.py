1434e922f17abc7ccbc142568aa4b43c48db4949
Annnvd <anna.van.dun@student.uva.nl>
2026-02-09 11:30:16 +0100

    Added boundary and initial conditions and parameters

---
+++ Assignment 1/assignment_2.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# boundary conditions
+c_y0 = 0
+c_y1 = 1
+
+# initial conditions
+c_t0 = 0
+
+# interval lengths
+N = 1000
+delta_x = 1 / N
+delta_y = 1 / N
+delta_t = 0.0001
+
+# parameters
+D = 1
+
