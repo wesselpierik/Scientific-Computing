91edaad5285623ce85c4b1fcbd404ac4c68aadaa
Annnvd <anna.van.dun@student.uva.nl>
2026-02-09 11:52:59 +0100

    Created function for a single time step.

--- Assignment 1/assignment_2.py
+++ Assignment 1/assignment_2.py
@@ -9,13 +9,20 @@ c_y1 = 1
 c_t0 = 0
 
 # interval lengths
-N = 1000
+N = 10
+t0 = 0
+tN = 100
 delta_x = 1 / N
 delta_y = 1 / N
 delta_t = 0.0001
 
 # parameters
 D = 1
+
+# array with x and y
+c = np.zeros((N, N))
+c[0,] = c_y1
+print(c)
 
 def stability_condition(delta_t, delta_x, D):
     stability_check = (4 * delta_t * D) / (delta_x ** 2)
@@ -25,3 +32,18 @@ def stability_condition(delta_t, delta_x, D):
     
     print(f"The stability condition is not satisfied, value is: {stability_check}")
     return False
+
+def concentration_timestep():
+    for x, y in range(delta_x):
+
+        # Boundary condition
+        if y == 0 or y == delta_x - 1:
+            continue
+
+        if x == delta_x - 1:
+            c[y, x] = c[y, x] + delta_t * D / (delta_x ** 2) * (c[y, 0] + c[y, x-1] + c[y+1, x] + c[y-1, x] - 4*c[y, x])
+
+        else:
+            c[y, x] = c[y, x] + delta_t * D / (delta_x ** 2) * (c[y, x+1] + c[y, x-1] + c[y+1, x] + c[y-1, x] - 4*c[y, x])
+
+    return c
