e89bceba99e3797effc4705bcab8a0f8c6e3a7e5
MikeNieuweboer <nieuweboermike@gmail.com>
2026-02-10 16:48:13 +0100

    Finished core dla logic

--- Assignment 2/src/dla.py
+++ Assignment 2/src/dla.py
@@ -14,38 +14,97 @@ from numba import njit
 
 
 class DLA:
-    def __init__(self, grid_size: int, *, omega: float = 1) -> None:
+    def __init__(
+        self, grid_size: int, eta: float, *, omega: float = 1, seed: int = 43
+    ) -> None:
         if grid_size <= 2:  # noqa: PLR2004
             size_error = "Grid size is too small for any meaningful calculation."
             raise ValueError(size_error)
 
-        self._growths = np.zeros((grid_size, grid_size))
-        self._candidates = np.zeros((grid_size, grid_size))
+        self._gen = np.random.Generator(np.random.PCG64(seed))
+
+        self._growths = np.zeros((grid_size, grid_size), dtype=np.bool)
+        self._candidate_list = []
+        self._candidate_array = np.zeros((grid_size, grid_size), dtype=np.bool)
         self._nutrients = np.zeros((grid_size, grid_size))
+
         self._grid_size = grid_size
         self._omega = omega
+        self._eta = eta
+
+        self.reset_grid()
+
+    def reset_grid(self) -> None:
+        """Reset the initial conditions for the nutrients, candidates and growths."""
+        middle = int(self._grid_size / 2)
+        # Empty growths and place seed at middle bottom
+        self._growths[:, :] = 0
+        self._growths[-1, middle] = 1
+
+        # Add new candidates
+        self._candidate_list = []
+        self._candidate_array[:, :] = False
+        self.add_candidates(self._grid_size - 1, middle)
+
+        # Reset nutrient concentrations
+        self._nutrients[1:, :] = 0
+        self._nutrients[0, :] = 1
+
+    def add_candidates(self, row: int, column: int) -> None:
+        for row_offset, column_offset in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
+            new_row = row + row_offset
+            new_column = column + column_offset % self._grid_size
+
+            # Don't add candidate if it falls outside, is occupied, or already added
+            if (
+                (new_row >= self._grid_size)
+                or (self._candidate_array[new_row, new_column])
+                or (self._growths[new_row, new_column])
+            ):
+                continue
+
+            self._candidate_list.append((new_row, new_column))
+            self._candidate_array[new_row, new_column] = True
 
     def stabilize_nutrients(self) -> None:
-        pass
+        epsilon = 1e-12
+        while self.step_nutrients() > epsilon:
+            pass
 
     def grow_candidate(self) -> None:
-        pass
+        # Calculate probabilites for each candidate dependent on nutrient concentration
+        concentrations = np.array(
+            [self._nutrients[row, column] for row, column in self._candidate_list]
+        )
+        concentrations = np.pow(concentrations, self._eta)
+        total_concentration = np.sum(concentrations)
+        probabilities = concentrations / total_concentration
 
-    def step(self) -> float:
+        # Grow out new candidate
+        index = self._gen.choice(range(len(self._candidate_list)), 1, p=probabilities)
+        row, column = self._candidate_list[index]
+        self._candidate_array[row, column] = False
+        self._growths[row, column] = True
+        self._candidate_list[index] = self._candidate_list[-1]
+        self._candidate_list.pop()
+
+    def step(self) -> None:
+        self.stabilize_nutrients()
+        self.grow_candidate()
+
+    def step_nutrients(self) -> float:
         """Perform one iteration step of the SOR diffusion solver.
-
-        Must be implemented by subclasses.
 
         Returns:
             Maximum difference between old and new values in the iteration,
             used for convergence detection.
 
         """
-        return self._step(self._nutrients, self._growths, self._omega)
+        return self._step_nutrients(self._nutrients, self._growths, self._omega)
 
     @staticmethod
     @njit
-    def _step(
+    def _step_nutrients(
         nutrients: npt.NDArray,
         growths: npt.NDArray,
         omega: float,
